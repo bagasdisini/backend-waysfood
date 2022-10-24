bab66e34cc7320ba0994bb87e600ffd469a5f2aa
Bagas <mbagas221@gmail.com>
2022-10-24 08:56:28 +0700

    test

--- dto/product/product_request.go
+++ dto/product/product_request.go
@@ -3,7 +3,7 @@ package productdto
 type CreateProductRequest struct {
 	Title string `json:"title" form:"title" gorm:"type: varchar(255)" validate:"required"`
 	Price int    `json:"price" form:"price" gorm:"type: int" validate:"required"`
-	Image string `json:"image" form:"image" gorm:"type: varchar(255)" validate:"required"`
+	Image string `json:"image" form:"image" gorm:"type: varchar(255)"`
 	Qty   int    `json:"qty" form:"qty" gorm:"type: int" validate:"required"`
 }
 
